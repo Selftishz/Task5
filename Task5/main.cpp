9768500aae98bde3d712051c5f4af94b1ec3a03b
Artyom <Artyom@DESKTOP-HB4QEA4>
2022-05-06 22:33:56 +0300

    Добавьте файлы проекта.

---
+++ Task5/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "Func.h"
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    size_t rows, columns;
+    std::cout << "Введите кол-во строк и столбцов: ";
+    std::cin >> rows >> columns;
+    int** A = Create(rows, columns);
+    FillRandomNumbers(A, rows, columns);
+    Print(A, rows, columns);
+    Free(A, rows);
+    return 0;
+}
